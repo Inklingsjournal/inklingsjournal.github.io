ae6b0d0685fce8d684a3a4b064966db3c7f56bb8
Inklings Journal <therapy.shreya@gmail.com>
2026-05-09 13:55:41 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'inklings-v1';
+const ASSETS = [
+  '/DigitalJournal/Login.html',
+  '/DigitalJournal/Code.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
